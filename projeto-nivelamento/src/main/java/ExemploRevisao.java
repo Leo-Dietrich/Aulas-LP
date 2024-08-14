bef56273f72fb9dceed678168985a155796529fc
Leo-Dietrich <leonardo.dietrich@sptech.school>
2024-08-14 17:47:51 -0300

    Projeto nivelamento, aula 2

---
+++ projeto-nivelamento/src/main/java/ExemploRevisao.java
@@ -0,0 +1,29 @@
+public class ExemploRevisao {
+    public static void main(String[] args) {
+        //Tipos mais utilizados
+        //Classes sempre começam com caixa alta ex String, Integer, Double, NomeRevisao etc
+
+        //Tipos Wrapper = são classes
+        //possuem métodos que ajudam
+        // As classes não possuem valor inicial, como uma váriavel, valor de wrapper = null
+        String nome = "Bob da Silva";
+        Integer idade = 22;
+        Double altura = 1.55;
+        Boolean bloqueado = false;
+
+        System.out.println
+                ("Meu nome é " + nome +
+                        ", minha altura é " + altura +
+                        ", e minha idade é " + idade);
+
+        String nomePet;
+        nomePet = "Teste";
+
+        //Não usaremos!!
+        //Tipo primitivo, não são classes, não possuem métodos
+        //Tipo primitivo sempre tem valor inicial
+        boolean bloqueado2 = false;
+        int numero = 42;
+
+    }
+}
